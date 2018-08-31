a1663531f687fbead6a6a12b0d4b35d8c676b28b
Soojeong <soojeongshin2@gmail.com>
2018-08-31 22:53:14 +0900

    feat: Create a Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.android.jokeprovider.JokeProvider;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeProvider jokeProvider = new JokeProvider();
+        Toast.makeText(this, jokeProvider.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ jokeprovider/src/main/java/com/example/android/jokeprovider/JokeProvider.java
@@ -0,0 +1,8 @@
+package com.example.android.jokeprovider;
+
+public class JokeProvider {
+
+    public String getJoke() {
+        return "This is a joke from a Java Library";
+    }
+}
