efff68be339053b6fbd8f9bcec5eb577c7c76620
Soojeong <soojeongshin2@gmail.com>
2018-09-10 11:52:15 +0900

    feat: Add NotNull annotation

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.util.Pair;
 import android.support.v7.widget.LinearLayoutManager;
@@ -59,7 +60,7 @@ public class MainActivityFragment extends Fragment implements CategoryAdapter.Ca
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Instantiate FragmentMainBinding using DataBindingUtil
         mFragmentMainBinding = DataBindingUtil.inflate(
